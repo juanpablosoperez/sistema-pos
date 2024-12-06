9fb3bf51891cc7cdacaea1d5b79ce1ebc070bb2e
Tomib4n <tbanchio15@gmail.com>
2024-12-06 13:44:25 -0300

    refactor: new folder structure

---
+++ src/models/models.py
@@ -0,0 +1,17 @@
+from peewee import CharField
+from peewee import Model
+
+from models.connection import db
+
+
+class User(Model):
+    username = CharField(unique=True)
+    password = CharField()
+
+    class Meta:
+        database = db
+
+
+def create_tables():
+    with db:
+        db.create_tables([User])
